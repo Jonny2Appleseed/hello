83849f1b88ee580bacb4fca43285558f507e1aa3
Jonny2Appleseed <jfrank13@huskers.unl.edu>
2024-09-02 17:00:55 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Jonathan Frank
+ *         jfrank13@huskers.unl.edu
+ * Date: 2024/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jonathan Frank!\n");
+
+  printf("I will be a Computer Engineer!\n");
+
+  return 0;
+}
