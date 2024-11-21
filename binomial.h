3d1c67aaaac71684e3df3e30b8bb22384f4e186a
ctice4 <ctice4@huskers.unl.edu>
2024-11-21 09:55:46 -0600

    starter code

---
+++ binomial.h
@@ -0,0 +1,11 @@
+/**
+ * Author: Carson Tice and Jonathan frank
+ * Date: 2024-11-21
+ *
+ * This file holds the prototypes and the definitions of functions
+ */
+
+/**
+ * This function
+ */
+long choose(int n, int k);
---
+++ binomialDemo.c
@@ -0,0 +1,26 @@
+/**
+ * Author: Carson Tice and Jonathan frank
+ * Date: 2024-11-21
+ *
+ * This file will run the functions from binomial.c
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "binomial.h"
+
+
+int main(int argc, char **argv){
+
+if (argc != 3) {
+    exit(1);
+  }
+  int n = atoi(argv[1]);
+  int k = atoi(argv[2]);
+
+
+
+
+}
